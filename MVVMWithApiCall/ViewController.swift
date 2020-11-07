6ef107bab2362f528e88eba0c6f7783a0a991d50
Zabeehullah Qayumi <zabeeh.qayumi@gmail.com>
2020-11-06 19:10:59 -0800

    Initial Commit

---
+++ MVVMWithApiCall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMWithApiCall
+//
+//  Created by Zab on 11/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
