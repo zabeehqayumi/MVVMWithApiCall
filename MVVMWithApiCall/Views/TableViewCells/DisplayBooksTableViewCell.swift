c88a5490136d831bd498c009677a04426849252b
Zabeehullah Qayumi <zabeeh.qayumi@gmail.com>
2020-11-09 18:47:28 -0800

    Added Cells

---
+++ MVVMWithApiCall/Views/TableViewCells/DisplayBooksTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DisplayBooksTableViewCell.swift
+//  MVVMWithApiCall
+//
+//  Created by Zab on 11/9/20.
+//
+
+import UIKit
+
+class DisplayBooksTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
